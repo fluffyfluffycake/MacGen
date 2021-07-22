246a7b3284777af0d1c8d5faa4509c1e970d2d66
fluffyfluffycake <87567639+fluffyfluffycake@users.noreply.github.com>
2021-07-22 15:27:53 +0100

    Create MacGen.cpp
    MAC address prefix(Apple)

--- MacGen.cpp
+++ MacGen.cpp
@@ -7,7 +7,8 @@ int main()
 {
   std::string hex_char [16] = {"1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f"};
   srand(time(NULL));
-  for(int i=0; i<=11; i++)
+  std::cout << "f81edf";
+  for(int i=0; i<=5; i++)
   {
     std::cout<<hex_char [rand() % 16];
   }
