581743347d7183da0d63e80578a83bf9aa4b939d
fluffyfluffycake <87567639+fluffyfluffycake@users.noreply.github.com>
2021-07-22 16:02:00 +0100

    Update MacGen.cpp
    Randomised Mac address prefix

--- MacGen.cpp
+++ MacGen.cpp
@@ -6,8 +6,9 @@
 int main() 
 {
   std::string hex_char [16] = {"1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f"};
+  std::string prefix [5] = {"f81edf","4cf95d","00a00d","00001a","c8ddc9"};
   srand(time(NULL));
-  std::cout << "f81edf";
+  std::cout << prefix[rand() % 5];
   for(int i=0; i<=5; i++)
   {
     std::cout<<hex_char [rand() % 16];
